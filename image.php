130e25f96a313c43675decdb28fb9d6793f8400e
zruzak <zeljko.ruzak@gmail.com>
2019-01-28 15:39:53 +0100

    Force green

--- image.php
+++ image.php
@@ -5,13 +5,8 @@ $white = imagecolorallocate($im, 255, 255, 255);
 
 $deployment = getenv("COLOR");
 
-if ( $deployment == 'blue') {
-  $color = imagecolorallocate($im, 0, 0, 255);
-} elseif ($deployment == 'green')  {
-  $color = imagecolorallocate($im, 0, 255, 0);
-} else {
-  $color = imagecolorallocate($im, 0, 0, 255);
-}
+$color = imagecolorallocate($im, 0, 255, 0);
+
 
 // Draw a filled rectangle
 imagefilledrectangle($im, 0, 0, 199, 199, $color);
